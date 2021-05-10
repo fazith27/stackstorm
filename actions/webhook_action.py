e1b1ab9943123bfd54240459ccec15071f02d8e9
Fazith <fazith27@gmail.com>
2021-05-10 16:28:30 +1000

    Testing my pack

---
+++ actions/webhook_action.py
@@ -0,0 +1,12 @@
+import json
+from st2common.runners.base_action import Action
+
+
+class MyWebhookAction(Action):
+    
+    def run(self, payload, **params):
+        payload = json.dumps(payload).encode()
+        payload["name"] = params["name"]
+        payload["type"] = params["type"]
+        payload["base"] = params["base"]
+        print(payload)
